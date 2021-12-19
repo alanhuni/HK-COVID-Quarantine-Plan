cc5bf66e645e975dfd0a4aeb1e20643e0acd891a
aLaNHuNi <37041271+alanhuni@users.noreply.github.com>
2021-12-19 21:08:52 +0800

    Add files via upload

---
+++ quarantineplanner.py
@@ -0,0 +1,53 @@
+hotels = [['Best Western Plus Hotel Hong Kong', 'Central & Western', '308 Des Voeux Road West', 'http://www.bestwestern plushotelhongkong.com', 'hotel@bestwesternplush otel.com', 34103333], ['CM+ Hotels & Serviced Apartments', 'Central & Western', 'No. 16\nConnaught Road West,\nHong Kong', 'https://cmplus.com.hk/e n/quarantine-package/', 'cmplus@cmpd.com.hk', 35606738], ['Courtyard by Marriott Hong Kong', 'Central & Western', '167 Connaught Road West, Hong Kong', 'www.courtyardhongkon g.com', 'cy.hkgcy.reservations@ courtyard.com', 37178888], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ['Grand City Hotel', 'Central & Western', "338 Queen's Road West", 'http://www.grandcityhot elhongkong.com', 'hotel@grandcityhotelho ngkong.com', 21921222], ['JEN Hong Kong by Shangri-La', 'Central & Western', "508 Queen's Road West, Hong Kong SAR", 'https://www.shangri- la.com/en/hotels/jen/ho ngkong/westerndistrict', 'csteam.hongkong@shan gri-la.com', 27338786], ['Lan Kwai Fong Hotel @ Kau U Fong', 'Central & Western', 'No.3 Kau U Fong, Central, Hong Kong', 'https://www.dcollection. com/zh-hk/lan-kwai- fong-hotel-kau-u-fong/', 'rsvn@lankwaifonghotel. com.hk', 36500299], ['One-Eight-One Hotel & Serviced Residences', 'Central & Western', '181 Connaught Road West', 'www.one-eight-one.com', 'enquiries@one-eight- one.com', 31811688], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ['Ovolo Central', 'Central & Western', '2 Arbuthnot Road, Central, Hong Kong', 'https://ovolohotels.com/ ovolo/offers/meet-your- quarantine-concierge/', 'reservations@ovologrou p.com', 21651000], ['Ramada Hong Kong Harbour View', 'Central & Western', "239 Queen's Road West Hong Kong", 'http://www.ramadahong kongharbourview.com/', 'rsvn@ramadahkhv.com', 25999888], ['The Landmark Mandarin Oriental Hong Kong', 'Central & Western', "The Landmark, 15 Queen's Road Central, Hong Kong", 'https://www.mandarinor iental.com/hong- kong/the- landmark/luxury-hotel', 'anitangan@mohg.com', 21320188], ['iclub Fortress Hill Hotel', 'Eastern', '18 Merlin Street, North Point', 'https://www.iclub- hotels.com/iclub- fortress-hill- hotel/en/home/home.ht ml', 'servicedesk@fortresshil l.iclub-hotels.com', 39636300], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ['Ramada Hong Kong Grand View', 'Eastern', 'No.88 Chun Yeung Street, North Point, Hong Kong', 'www.ramadahongkongg randview.com', 'hotel@ramadahkgv.com', '2130\n3388'], ["Nina Hotel Island South (Formerly L'hotel Island South)", 'Southern', '55 Wong Chuk Hang Road, Aberdeen, Hong Kong', 'https://www.ninahotelgr oup.com/en/nina-hotel- island-south/quarantine', 'reservations.is@ninahot elgroup.com', 39688838], ['Ovolo Southside', 'Southern', '64 Wong Chuk Hang Road, Wong Chuk Hang', 'https://ovolohotels.com/ ovolo/offers/meet-your- quarantine-concierge/', 'reservations@ovologrou p.com', 21651000], ['Best Western Hotel Causeway Bay', 'Wan Chai', '38 Bowrington Road, Causeway Bay', 'http://www.bestwestern hotelhongkong.com', 'rsvn@bestwesternhotel hongkong.com', 24966666], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ['Crowne Plaza Hong Kong Causeway Bay', 'Wan Chai', '8 Leighton Road, Causeway Bay, Hong Kong', 'www.cphongkong.com', 'rsvn@cphongkong.com', 39803980], ['Dorsett Wanchai, Hong Kong', 'WanChai', "387 - 397 Queen's Road East, Wan Chai, Hong Kong", 'https://www.wanchai.do rsetthotels.com/', 'reservation.wanchai@d orsetthotels.com', '852 3552\n1133'], ['Eco Tree Hotel Causeway Bay', 'Wan Chai', '15 Mercury Street, Causeway Bay, Hong Kong', 'https://causewaybay.eco treehotel.com.hk/home/', 'reservation@ecotreehot elcwb.com.hk', 23171338], ['Empire Hotel Causeway Bay', 'Wan Chai', 'No. 8 Wing Hing Street, Hong Kong', 'http://www.empirehotel. com.hk', 'reservations- cwb@empirehotel- hongkong.com', '3692\n2333'], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ['Empire Prestige Causeway Bay', 'Wan Chai', 'No. 8A Wing Hing Street, Hong Kong', 'http://www.empirehotel. com.hk', 'reservations- cwb@empirehotel- hongkong.com', '3692\n2333'], ['Hotel Indigo Hong Kong Island', 'Wan Chai', "No. 246 Queen's Road East", 'www.hotelindigo.com/h ongkong', 'rsvn.hkgin@ihg.com', 39263888], ['Lanson Place Causeway Bay, Hong Kong\n(Commence operations from 28 November 2021)', 'Wan Chai', '133 Leighton Road, Causeway Bay, Hong Kong', 'https://lansonplace.com/ causewaybay/', 'enquiry.lphk@lansonpla ce.com', 34776888], ['Mira Moon Hotel', 'Wan Chai', '388 Jaffe Road, Causeway Bay, Hong Kong', 'https://www.miramoonh otel.com', 'enquiry@miramoonhote l.com', 26438888], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ['Bridal Tea House Hotel Hung Hom Gillies Avenue South', 'Kowloon City', 'No. 69 Gillies Avenue South, Hung Hom', 'https://bthhotel.com', 'info@bthhotel.com', 23641212], ['iclub Ma Tau Wai Hotel', 'Kowloon City', '8 Ha Heung Road, Ma Tau Wai, Hong Kong', 'https://www.iclub- hotels.com/iclub-ma- tau-wai-hotel', 'servicedesk@matauwai. iclub-hotels.com', 39636600], ['Kerry Hotel, Hong Kong', 'Kowloon City', '38 Hung Luen Road, Hung Hom Bay, Kowloon Hong Kong SAR', 'https://www.shangri- la.com/en/hongkong/ker ry/', 'csteam.hongkong@shan gri-la.com', 27338786], ['Metropark Hotel Kowloon', 'Kowloon City', '75 Waterloo Road, Kowloon, Hong Kong', 'www.MetroparkHotelK owloon.com', 'hotel.hkkl@metroparkh otels.com', 27611711], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ["O' Hotel", 'Kowloon City', '42 KOWLOON CITY ROAD', 'www.ohotel.hk', 'frontdesk@o- hotel.com.hk', 22319999], ['Regal Oriental Hotel', 'Kowloon City', '30 - 38 Sa Po\nRoad, Kowloon City, Hong Kong', 'https://www.regalhotel.c om/regal-oriental-hotel', 'roh.foam@regalhotel.co m', 61179319], ['Pentahotel Hong Kong, Kowloon', 'Wong Tai Sin', '19 Luk Hop Street, San Po Kong, Kowloon, Hong Kong', 'www.pentahotelhongko ngkowloon.com', 'reservations.kowloon@ pentahotelsasia.com', 31128222], ['Bridal Tea House Hotel Yau Ma Tei Wing Sing Lane', 'Yau Tsim Mong', 'No. 5-5C Wing Sing Lane, Yau Ma Tei, Kowloon', 'https://bthhotel.com', 'info@bthhotel.com', 37287000], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ['Dorsett Mongkok Hong Kong', 'Yau Tsim Mong', '88 Tai Kok Tsui Road, Kowloon, Hong Kong', 'https://www.mongkok.d orsetthotels.com/', 'reservation.mongkok@d orsetthotels.com', '852 3987\n2388'], ['Holiday Inn Golden Mile Hong Kong\n(Commence operations from 30 November 2021)', 'Yau Tsim Mong', '50 Nathan Road, Tsim Sha Tsui, Kowloon, Hong Kong', 'https://www.ihg.com/ho lidayinn/hotels/us/en/ho ng- kong/hkgkl/hoteldetail', 'reservations.HIGM@ih g.com', 23151001], ['Ramada Hong Kong Grand', 'Yau Tsim Mong', '23 Austin Avenue, Tsim Sha Tsui, Kowloon, Hong Kon', 'www.ramadahongkongg rand.com', 'rsvn@ramadahkg.com', 31226222], ['Silka Seaview Hotel Hong Kong', 'Yau Tsim Mong', '268 Shanghai Street, Yau Ma Tei,\nKowloon, Hong Kong', 'www.silkahotels.com/se aview', 'info.seaview@silkahotel s.com', 27820882], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ['Dorsett Tsuen Wan, Hong Kong', 'Kwai Tsing District', '28 Kin Chuen Street, Kwai Chung, Hong Kong', 'https://www.dorsetthote ls.com/zh-hk/dorsett- tsuen- wan/offers/4_14_min16\n_nights_special.html', 'reservations.tsuenwan@ dorsetthotels.com', '3996\n6863'], ['Four Points by Sheraton Hong Kong, Tung Chung', 'Islands', '9 Yi Tung Road, Tung Chung, Lantau Island, Hong Kong', 'https://www.marriott.co m/hotels/hotel- deals/details/hkgfp- four-points-hong-kong- tung-chung/off-19568', 'fp.hkgfp.reservations@f ourpoints.com', 23528035], ['Regal Airport Hotel', 'Islands', '9 Cheong Tat Road, Hong Kong International Airport, Chek  Lap Kok, Hong Kong', 'www.regalhotel.com/reg al-airport-hotel', 'stay@airport.regalhotel. com', 22866888], ['Nina Hotel Tsuen Wan West (Tower 1)', 'Tsuen Wan', '8 Yeung Uk Rd, Tsuen Wan', 'https://www.ninahotelgr oup.com/en/nina-hotel- tsuen-wan-west', 'qt.tww@ninahotelgroup\n.com', 22802898], ['Name of Hotel', 'District', 'Address', 'Website', 'Email', 'Phone No.'], ['Silka Far East Hotel Hong Kong', 'Tsuen Wan', '135 - 143 Castle Peak Road, Tsuen Wan, New Terrorites', 'www.silkahotels.com/fa reast', 'info.fareast@silkahotels\n.com', 24069090]]
+risk2_countries = ["Argentina", "Australia", "Austria", "Bangladesh", "Brazil", "Belgium", "Cambodia", "Canada", "Chile", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Fiji", "Finland", "France", "Germany", "Ghana", "Greece", "Iceland", "India", "Indonesia", "Iran", "Ireland", "Israel", "Italy", "Japan", "Jordan", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Latvia", "Lebanon", "Liechtenstein", "Malaysia", "Maldives", "Mauritius", "Mexico", "Nepal", "Netherlands", "Norway", "Pakistan", "Philippines", "Portugal", "Romania", "Russia", "Saudi Arabia", "Senegal", "Sierra Leone", "Singapore", "Spain", "Sri Lanka", "Sweden", "Switzerland", "Tanzania", "Thailand", "Tunisia", "Turkey", "Uganda", "United Arab Emirates", "United Kingdom"]
+risk3_countries = ["Macao", "China", "Hong Kong", "Taiwan"]
+risklevel = 0 #0 is no risk, 1 is medium, 2 is high, 3 is exception
+
+def sortrisk(country):
+    if country in risk2_countries:
+        risklevel = 2
+    elif country in risk3_countries:
+        risklevel = 3
+    else:
+        risklevel = 1
+    return risklevel
+
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask (__name__)
+@app.route("/", methods=["GET","POST"])
+def home():
+	if request.method=="POST":
+		country = request.form["country"]
+		print (country)
+		if sortrisk(country) == 1:
+			return redirect(url_for("risk1"))
+		elif sortrisk(country) == 2:
+			return redirect(url_for("risk2"))
+		elif sortrisk(country) == 3:
+			return redirect(url_for("risk3"))
+		else:
+			return render_template("home.html")
+	else:	
+		return render_template("home.html")
+
+@app.route("/risk1")
+def risk1():
+	return render_template("risk1.html")
+
+@app.route("/risk2")
+def risk2():
+	return render_template("risk2.html")
+
+@app.route("/risk3")
+def risk3():
+	return render_template("risk3.html")
+
+@app.route("/hotels")
+def hotels():
+	return render_template("hotels.html", hotel=hotels)
+
+
+#start website
+if __name__ == "__main__":
+	app.run(debug=True,host="localhost")
